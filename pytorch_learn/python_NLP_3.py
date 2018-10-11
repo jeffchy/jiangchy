14a7a71a0b2f76916aae269e6a2336815fcb5e34
Jeff Chiang <jiangchy@shanghaitech.edu.cn>
2018-10-11 12:29:35 +0800

    add pytorch learn nlp

---
+++ pytorch_learn/python_NLP_3.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
+EMBEDDING_DIM = 5
+
+raw_text = """We are about to study the idea of a computational process.
+Computational processes are abstract beings that inhabit computers.
+As they evolve, processes manipulate other abstract things called data.
+The evolution of a process is directed by a pattern of rules
+called a program. People create programs to direct processes. In effect,
+we conjure the spirits of the computer with our spells.""".split()
+
+# By deriving a set from `raw_text`, we deduplicate the array
+vocab = set(raw_text)
+vocab_size = len(vocab)
+
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+data = []
+for i in range(2, len(raw_text) - 2):
+    context = [raw_text[i - 2], raw_text[i - 1],
+               raw_text[i + 1], raw_text[i + 2]]
+    target = raw_text[i]
+    data.append((context, target))
+print(data[:5])
+
+
+class CBOW(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, context_size):
+        super(CBOW, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear = nn.Linear(embedding_dim, vocab_size)
+
+    def forward(self, inputs):
+        # input is tensor for 2 x CONTEXT_SIZE
+        embeds = self.embeddings(inputs)
+        embeds_sum = torch.sum(embeds, dim = 0).view((1, -1))
+        log_probs = F.log_softmax(self.linear(embeds_sum), dim=1)
+        return log_probs
+
+# create your model and train.  here are some functions to help you make
+# the data ready for use by your module
+
+def make_context_vector(context, word_to_ix):
+    idxs = [word_to_ix[w] for w in context]
+    return torch.tensor(idxs, dtype=torch.long)
+
+
+example = make_context_vector(data[0][0], word_to_ix)  # example
+print(example)
+
+losses = []
+loss_function = nn.NLLLoss()
+model = CBOW(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+for epoch in range(10):
+    total_loss = 0
+    for context, target in data:
+
+        # Step 1. Prepare the inputs to be passed to the model
+        target_idx = torch.tensor([word_to_ix[target]], dtype=torch.long)
+        context_idxs = make_context_vector(context, word_to_ix)
+
+        # Step 2. Recall that torch *accumulates* gradients. Before passing in a
+        # new instance, you need to zero out the gradients from the old
+        # instance
+        model.zero_grad()
+
+        # Step 3. Run the forward pass, getting log probabilities over next
+        # words
+        log_probs = model(context_idxs)
+
+        # Step 4. Compute your loss function. (Again, Torch wants the target
+        # word wrapped in a tensor)
+        # In NLLloss, it expect expected to contain log_probabilities of each class
+        loss = loss_function(log_probs, target_idx)
+
+        # Step 5. Do the backward pass and update the gradient
+        loss.backward()
+        optimizer.step()
+
+        # Get the Python number from a 1-element Tensor by calling tensor.item()
+        total_loss += loss.item()
+    losses.append(total_loss)
+
+print(losses)  # The losss
